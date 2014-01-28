6b9fc0b33164dc0c0ec08c8b5379edf382e4418d
Tom Adams <tom@dxw.com>
2014-01-28 11:46:50 -0500

    Oops

--- src/WPMissing/Pagination.php
+++ src/WPMissing/Pagination.php
@@ -8,6 +8,8 @@ class Pagination {
     $this->paged = ($this->query->get('paged')) ? $this->query->get('paged') : 1;
   }
 
+  // have_*
+
   function have_previous() {
     return $this->paged > 1;
   }
@@ -15,6 +17,8 @@ class Pagination {
   function have_next() {
     return $this->paged < $this->query->max_num_pages;
   }
+
+  // uri_*
 
   function uri_previous() {
     return $this->uri_for($this->paged - 1);
@@ -28,15 +32,19 @@ class Pagination {
     return get_pagenum_link($i, false);
   }
 
+  // link_*
+
   function link_previous($text) {
-    return sprintf('<a href="%s">%s</a>', $this->uri_previous(), $text);
+    return sprintf('<a href="%s">%s</a>', esc_attr($this->uri_previous()), $text);
   }
 
   function link_next($text) {
-    return sprintf('<a href="%s">%s</a>', $this->uri_next(), $text);
+    return sprintf('<a href="%s">%s</a>', esc_attr($this->uri_next()), $text);
   }
 
-  function numbers($context, $show_first, $show_last) {
+  // etc
+
+  function numbers($context, $show_first_last) {
     return 'todo';
   }
 }
