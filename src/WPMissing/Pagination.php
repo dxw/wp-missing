1d3d509edb0d8a71c3d8ef9b02843e2ba94e0b9e
Tom Adams <tom@dxw.com>
2014-01-28 13:37:13 -0500

    Numbers is mostly working

--- src/WPMissing/Pagination.php
+++ src/WPMissing/Pagination.php
@@ -20,10 +20,6 @@ class Pagination {
 
   // uri_*
 
-  function uri_previous() {
-    return $this->uri_for($this->paged - 1);
-  }
-
   function uri_next() {
     return $this->uri_for($this->paged + 1);
   }
@@ -34,17 +30,43 @@ class Pagination {
 
   // link_*
 
-  function link_previous($text) {
-    return sprintf('<a href="%s">%s</a>', esc_attr($this->uri_previous()), $text);
+  function link_for($i, $text=null) {
+    if ($text === null) {
+      $text = $i;
+    }
+    return sprintf('<a href="%s">%s</a>', esc_attr($this->uri_for($i)), esc_html($text));
   }
 
-  function link_next($text) {
-    return sprintf('<a href="%s">%s</a>', esc_attr($this->uri_next()), $text);
+  function link_previous($text=null) {
+    return $this->link_for($this->paged - 1, $text);
+  }
+
+  function link_next($text=null) {
+    return $this->link_for($this->paged + 1, $text);
   }
 
   // etc
 
   function numbers($context, $show_first_last) {
-    return 'todo';
+    $numbers = [];
+    for ($i = $this->paged - $context; $i <= $this->paged + $context; $i++) {
+      if ($i > 0 && $i <= $this->query->max_num_pages) {
+        $current = $i === $this->paged;
+        $numbers[] = sprintf('<span>%s</span>', $this->link_for($i));
+      }
+    }
+
+    $first = $last = '';
+
+    if ($show_first_last) {
+      if ($this->paged - $context > 1) {
+        $first = $this->link_for(1).' … ';
+      }
+      if ($this->paged + $context < $this->query->max_num_pages) {
+        $last = ' … '.$this->link_for($this->query->max_num_pages);
+      }
+    }
+
+    return $first.implode(' ', $numbers).$last;
   }
 }
